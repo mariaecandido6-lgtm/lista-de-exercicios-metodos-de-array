12417096a81a9efa7ee59167bf89ce0ec51a4079
mariaecandido6-lgtm <maria.e.candido6@edu.senai.br>
2026-08-27 11:56:08 -0300

    nivel 1

--- ex1.js
+++
@@ -1,4 +0,0 @@
-// Nível 1- Procurando uma frutas
-const frutas = ["Morango", "Banana", "Uva", "Abacaxi", "Limão"];
-console.log("Tem banana?", frutas.includes("Banana"));
-
---
+++ nivel1.js
@@ -0,0 +1,20 @@
+// Nível 1- Procurando uma frutas
+const frutas = ["Morango", "Banana", "Uva", "Abacaxi", "Limão"];
+console.log("Tem banana?", frutas.includes("Banana"));
+
+//2 - Verificando uma linguagem
+let linguagens = ['JavaScript', 'Python', 'Java', 'C#', 'PHP'];
+console.log("Existe JavaScript?", linguagens.includes("JavaScript"));
+console.log("Existe Python?", linguagens.includes(Java));
+console.log("Existe C++?", linguagens.includes(C++));
+
+//3 - Encontrando a posição
+const alunos = ["Emanuelly", "Gabriela", "Leticia", "Maria", "Paloma", "Pamela"]
+console.log("Posição de Maria:", alunos.indexOf("Maria"));
+
+//4 - Aluno não encontrado
+const posicao = alunos.indexOf("Gabriela");
+
+if (posicao !== -1) {
+    console.log
+}
